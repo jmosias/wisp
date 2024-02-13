ac781e11bef71255a067385703b270fece41eb2d
Jm Osias <jmosias@outlook.com>
2024-02-14 00:06:23 +0800

    Added image compression and image merging

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -2,19 +2,69 @@
 import { useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
+import mergeImages from "merge-images";
+import imageCompression from "browser-image-compression";
 
 export default function Home() {
-  const [preview_urls, set_preview_urls] = useState([]);
+  const [previewURLs, setPreviewURLs] = useState([]);
+  const [mergePreview, setMergePreview] = useState("");
 
-  const handleChange = (e) => {
-    set_preview_urls([]);
-    set_preview_urls((prev) =>
-      prev.concat(
-        Array.from(e.target.files ?? []).map((f) =>
-          window.URL.createObjectURL(f)
-        )
-      )
+  const handleChange = async (e) => {
+    setPreviewURLs(await compressImages(e.target.files));
+  };
+
+  const compressImages = async (imageFiles) => {
+    console.log("IMAGEFILES", imageFiles);
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 2048,
+      useWebWorker: true,
+    };
+
+    const compressedImages = await Promise.all(
+      Array.from(imageFiles).map(async (imageFile, index) => {
+        console.log(
+          `originalFile instanceof Blob: ${index}`,
+          imageFile instanceof Blob
+        );
+        console.log(
+          `originalFile size [Blob: ${index}] ${
+            imageFile.size / 1024 / 1024
+          } MB`
+        );
+
+        try {
+          const compressedImage = await imageCompression(imageFile, options);
+          console.log(
+            `compressedImage instanceof Blob: ${index}`,
+            compressedImage instanceof Blob
+          );
+          console.log(
+            `compressedImage size [Blob: ${index}] ${
+              compressedImage.size / 1024 / 1024
+            } MB`
+          );
+
+          return URL.createObjectURL(compressedImage);
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
+      })
     );
+
+    return compressedImages.filter((url) => url !== null);
+  };
+
+  const handleMerge = () => {
+    const size = 2048;
+    mergeImages(
+      [
+        { src: previewURLs[0], x: 0, y: 0 },
+        { src: previewURLs[1], x: size / 2, y: 0 },
+      ],
+      { width: size, height: size }
+    ).then((src) => setMergePreview(src));
   };
 
   return (
@@ -34,9 +84,15 @@ export default function Home() {
         </form>
       </div>
       <div className="input-images">
-        {preview_urls.map((url) => (
+        {previewURLs.map((url) => (
           <Image key={url} src={url} alt="" width={150} height={200} />
         ))}
+      </div>
+      <button onClick={handleMerge}>MERGE</button>
+      <div>
+        {mergePreview && (
+          <Image src={mergePreview} alt="" width={300} height={300}></Image>
+        )}
       </div>
     </main>
   );
